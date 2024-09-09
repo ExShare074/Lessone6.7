a6acebd5bce0d073ca5d0a8f65a36e2de3c06477
ExShare074 <168080573+ExShare074@users.noreply.github.com>
2024-09-09 14:34:09 +0300

    add new code

---
+++ main.py
@@ -0,0 +1,13 @@
+import sqlite3
+
+conn = sqlite3.connect('example.db')
+cur = conn.cursor()
+cur.execute('''
+    CREATE TABLE IF NOT EXISTS users
+    (id INTEGER PRIMARY KEY, 
+    name TEXT NOT NULL, 
+    age INTEGER NOT NULL)
+''')
+conn.commit()
+
+cur.execute("INSERT INTO users (name, age) VALUES (?, ?)", ("Alexey", 25))
